9fdc2214a437875b16e22d4c28a3d4b6106dc9da
Daniel Noble <daniel.noble@anu.edu.au>
2024-03-05 09:29:40 +1100

    updating docs and descript

--- R/build_functions.R
+++ R/build_functions.R
@@ -1,9 +1,9 @@
 
-#| @title build_formula
-#| @description Function takes a string specifying a formula and converts this to a formula object to be used in the models
-#| x A character strong specifying the formula one wishes to use in the model
-#| @return A formula object
-#| @export
+#' @title build_formula
+#' @description Function takes a string specifying a formula and converts this to a formula object to be used in the models.
+#' @param x A character strong specifying the formula one wishes to use in the model
+#' @return A formula object
+#' @export
 
 build_formula <- function(x) {
 
