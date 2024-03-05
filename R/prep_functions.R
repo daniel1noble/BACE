36fe62dccbeb59c837cfba41cb144d4b874d5fde
Daniel Noble <daniel.noble@anu.edu.au>
2024-03-05 12:15:10 +1100

    wrote get variables function so that we can take formulas for fixed effects and then get the names of variables in the process

--- R/prep_functions.R
+++ R/prep_functions.R
@@ -11,3 +11,24 @@ get_class <- function(x) {
   return(lapply(x, class))	
 }
 
+#' @title get_variables
+#' @description Function takes a formula string and identifies the variables in the formula that relate to the data
+#' @param x A string
+#' @return A vector of variable names. This vector is used to subset out the 'fixed effects' and 'random effect' columns within a given dataframe. 
+#' @examples {
+#' # All should return the same three variables: y, x1, x2
+#' form <- "y ~ x1 + x2"
+#' get_variables(form)
+#' form <- "y ~ x1 * x2"
+#' get_variables(form)
+#' form <- "y ~ x1 + x2 + x1 : x2"
+#' get_variables(form)
+#' form <- "y ~ x1 + x2 + x1:x2"
+#' get_variables(form)
+#' form <- "y ~ x1 + x2 + x1*x2"
+#' get_variables(form)}
+#' @export
+get_variables <- function(x) {
+	vars <- unique(unlist(strsplit(x, "\\W+")))
+  return(vars)
+}
