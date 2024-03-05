942fa17b508962f8a5f9787a9254982549013f88
Daniel Noble <daniel.noble@anu.edu.au>
2024-03-05 15:35:06 +1100

    Need to work on identifying count variables from continuous but couple functions to classify variables in a dataframe

--- R/prep_functions.R
+++ R/prep_functions.R
@@ -32,3 +32,42 @@ get_variables <- function(x) {
 	vars <- unique(unlist(strsplit(x, "\\W+")))
   return(vars)
 }
+
+#' @title get_type
+#' @description Function takes a dataframe and identifies the class of variables (type)
+#' @param fixed_form The fixed effect formula
+#' @param cluster_name The name of the cluster variable
+#' @param random_form The random effect formula
+#' @return A list of variable names and their correpsonding class. 
+#' @examples {
+#' data(iris)}
+#' @export
+#' 
+
+get_type <- function(data) {
+ 
+  types  <- lapply(data, function(x) check_type(x))
+
+  return(types)
+
+}
+
+#' @title check_type
+#' @description Function takes a variable in a dataframe and checks/ classifies what tope
+#' @param x A vector of data
+#' @return Returns a character string specifying the class of the variable. 
+#' @export
+ 
+check_type <- function(x) {
+        if(length(unique(x[!is.na(x)])) == 2){type <- "binary"}
+
+		if(is.numeric(x)){type <- "continuous"}
+
+		if(is.integer(x)){type <- "count"} # Need to work on identifying counts from continuous
+    
+		if(is.factor(x) && length(levels(x)) > 2){type <- "categorical"}
+      
+	    if(is.ordered(x)){type <- "ordered_categorical"}
+  
+  	return(type)
+}
