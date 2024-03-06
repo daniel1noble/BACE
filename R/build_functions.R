32b8e2434ced3231d4209871c5bd49a4fd2d1847
Daniel Noble <daniel.noble@anu.edu.au>
2024-03-06 16:06:29 +1100

    formula function

--- R/build_functions.R
+++ R/build_functions.R
@@ -13,6 +13,33 @@ build_formula <- function(x) {
   return(stats::as.formula(x))
 }
 
+#' @title build_formula_string
+#' @description Function takes a string specifying a formula and creates all combinations of models using the variables.
+#' @param x A character string specifying the formula used in the model. This should be of the form: y ~ x.
+#' @return A list of formulas
+#' @export
+
+build_formula_string <- function(x) {
+
+	# Some checks. Formula must have a '`~`' in it
+	if(!grepl("~", as.character(x))) { stop("Make sure you specify a formula string. This involves a structure of the form: y ~ x") } 
+
+	vars <- get_variables(x) 
+
+	formulas <- list()
+	# Create all combinations of the variables
+	for(i in 1:length(vars)) {
+
+	 	if(i == 1) { formulas[[i]] <- x}
+		else{
+		formulas[[i]] <-  paste(vars[i], "~", paste(vars[-i], collapse = " + "))
+	}}
+
+  return(lapply(formulas, function(x) stats::as.formula(x)))
+}
+
+
+
 
 
 
