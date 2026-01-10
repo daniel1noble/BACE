cfa90bd153479a3dc0a90f246a53a567961c15af
Daniel Noble <daniel.noble@anu.edu.au>
2026-01-10 08:49:42 -0700

    fix

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,21 @@
 
     ver <- "0.0.0.9000"
 
-    loadmsg <- paste0("\nLoading the 'BACE' package (version ", ver, "). For an\nintroduction and vignette to the package please see: https://daniel1noble.github.io/BACE/\n
-    
-    To cite the package, use: citation('BACE')\n")
+    loadmsg <- paste0("\nLoading the 'BACE' package (version ", ver, "). For an\nintroduction and vignette to the package please see: https://daniel1noble.github.io/BACE/")
 
     packageStartupMessage(loadmsg, domain=NULL, appendLF=TRUE)
+    
+    # Print citation information
+    packageStartupMessage("\nTo cite BACE in publications, use:\n\n", domain=NULL, appendLF=FALSE)
+    cit <- utils::citation("BACE")
+    
+    # Format each citation with proper spacing
+    for (i in seq_along(cit)) {
+      packageStartupMessage(format(cit[i], style = "text"), domain=NULL, appendLF=FALSE)
+      if (i < length(cit)) {
+        packageStartupMessage("\n\n", domain=NULL, appendLF=FALSE)
+      }
+    }
+    packageStartupMessage("", domain=NULL, appendLF=TRUE)
 
   }
